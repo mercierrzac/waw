38070adb8b098075fbd839f1aa9b1e9f5b9a9afa
mercierrzac <123979852+mercierrzac@users.noreply.github.com>
2023-02-03 16:16:21 -0500

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,67 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . # #
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . # #
+        . . . # #
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . # #
+        . . . # #
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . # #
+        . . . # #
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . # #
+        . . . # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . # .
+        . # . . .
+        . . . . .
+        . . . # .
+        . # . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        . # . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # # . . .
+        # # . . .
+        `)
+    basic.pause(100)
+    basic.showString("waw")
+})
